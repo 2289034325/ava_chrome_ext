f04e16113abc5797b079d305175bfa856887ecb8
wc <2289034325@qq.com>
2019-08-10 17:06:15 +0800

    chrome 选词插件

---
+++ contentscript.js
@@ -0,0 +1,45 @@
+// Add bubble to the top of the page.
+var bubbleDOM = $('<div class="selection_bubble"><div class="inner_bubble"></div></div>')[0];
+document.body.appendChild(bubbleDOM);
+
+console.log("loaded");
+
+bubbleDOM.onclick = function(e){
+    console.log(bubbleDOM.getAttribute('sel_text'));
+    $.get("http://localhost:9000/dictionary/word/spider/chrome_extension?key=746819&lang=1&spell="+bubbleDOM.getAttribute('sel_text'),{},function(result){
+        console.log(result);
+        bubbleDOM.style.visibility = 'hidden';
+    });
+};
+bubbleDOM.onmouseup = function(e){
+    e.stopPropagation();
+};
+bubbleDOM.onmousedown = function(e){
+    e.stopPropagation();
+};
+
+
+// Lets listen to mouseup DOM events.
+document.addEventListener('mouseup', function (e) {
+    console.log(window.scrollY);
+    var selection = window.getSelection().toString();
+    if (selection.length > 0) {
+        renderBubble(e.clientX+window.scrollX, e.clientY+window.scrollY+20, selection);
+    }
+}, false);
+
+
+// Close the bubble when we click on the screen.
+document.addEventListener('mousedown', function (e) {
+    bubbleDOM.style.visibility = 'hidden';
+}, false);
+
+
+// Move that bubble to the appropriate location.
+function renderBubble(mouseX, mouseY, selection) {
+    console.log(mouseX,mouseY);
+    bubbleDOM.setAttribute('sel_text',selection);
+    bubbleDOM.style.top = mouseY + 'px';
+    bubbleDOM.style.left = mouseX + 'px';
+    bubbleDOM.style.visibility = 'visible';
+}
